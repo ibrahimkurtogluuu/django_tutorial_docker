2c4b095074edc318f0d749be75d8201e31779e1d
ibrahim kurtoğlu <ibrahimkurtoglu@192.168.1.7>
2025-03-17 18:16:35 +0300

    sectors and customer relations added.

--- grc/admin.py
+++ grc/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
-from .models import Standard, Customer
+from .models import Standard, Customer, Sector
 
-# Register your models here.
+class CustomerAdmin(admin.ModelAdmin):
+    filter_horizontal = ('sectors',)  # Makes sector selection more user-friendly
+    list_display = ('name', 'get_sectors')
+    
+    def get_sectors(self, obj):
+        return ", ".join([sector.name for sector in obj.sectors.all()])
+    get_sectors.short_description = 'Sectors'
+
+class SectorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'get_customers')
+    
+    def get_customers(self, obj):
+        return ", ".join([customer.name for customer in obj.customer_set.all()])
+    get_customers.short_description = 'Customers'
+
 admin.site.register(Standard)
-admin.site.register(Customer)
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Sector, SectorAdmin)
--- grc/models.py
+++ grc/models.py
@@ -3,14 +3,21 @@ from django.db import models
 # Create your models here.
 
 class Standard(models.Model):
-    standard_name = models.CharField(max_length=200)
-    standard_description = models.TextField()
+    name = models.CharField(max_length=200)
+    description = models.TextField()
     
     def __str__(self):
-        return self.standard_name
+        return self.name
 
 class Customer(models.Model):
     name = models.CharField(max_length=200, default='Default Customer Name')
+    sectors = models.ManyToManyField('Sector', blank=True)
+    
+    def __str__(self):
+        return self.name
+
+class Sector(models.Model):
+    name = models.CharField(max_length=200)
     
     def __str__(self):
         return self.name
