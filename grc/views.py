7f1005b1eddade3da8e305b1571020ed0ce3cee1
ibrahim kurtoğlu <ibrahimkurtoglu@192.168.1.3>
2025-04-29 13:41:25 +0300

    deleted unused libraries

--- grc/views.py
+++ grc/views.py
@@ -1,6 +1,4 @@
 from google import genai
-
-
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
