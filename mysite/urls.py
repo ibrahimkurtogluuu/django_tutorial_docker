3bba8f41f4c6d2b13c290e0dd80f357684fce31d
ibrahim kurtoğlu <ibrahimkurtoglu@192.168.1.4>
2025-05-12 17:46:04 +0300

    added root url to redirect grc url

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-
+from django.views.generic import RedirectView
 urlpatterns = [
     path("", RedirectView.as_view(url="/grc/")),  # Redirect / to /grc/
     path("polls/", include("polls.urls")),
